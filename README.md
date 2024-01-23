02d35693d2ef02e12f5e8af9b7ce1407aaf9dc00
0nhc <thefoxfoxfox@outlook.com>
2024-01-23 15:59:20 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # graspnet_ros
-ROS Implementation of GraspNet
+
+ROS Implementation of [GraspNet](https://githjub.com/graspnet/graspnet-baseline)
+
+## Prerequisites
+* Anaconda3
+* Ubuntu 20.04
+* ROS Noetic
+
+## Installation
+
+Create Python Environment
+```sh
+conda create -n graspnet_ros python=3.8
+```
+
+Install graspnetAPI.
+```sh
+git clone https://github.com/graspnet/graspnetAPI.git
+cd graspnetAPI
+pip install .
+```
---
+++ scripts/demo.py
@@ -0,0 +1,23 @@
+#!/home/hzx/anaconda3/envs/graspnet/bin/python3
+
+import os
+import sys
+import numpy as np
+import open3d as o3d
+import argparse
+import importlib
+import scipy.io as scio
+from PIL import Image
+
+import torch
+from graspnetAPI import GraspGroup
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(ROOT_DIR, 'models'))
+sys.path.append(os.path.join(ROOT_DIR, 'dataset'))
+sys.path.append(os.path.join(ROOT_DIR, 'utils'))
+
+from graspnet import GraspNet, pred_decode
+from graspnet_dataset import GraspNetDataset
+from collision_detector import ModelFreeCollisionDetector
+from data_utils import CameraInfo, create_point_cloud_from_depth_image
